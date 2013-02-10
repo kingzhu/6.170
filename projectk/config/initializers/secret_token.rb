00aac72cfe337acb0ba5e6e912a636db8c825543
King <king000@mit.edu>
2013-02-09 22:31:31 -0800

    sdesds

---
+++ projectk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectk::Application.config.secret_token = 'cb2664834fb7224cbc78ea5a6df6c9f72b2bf29e218bb0f487a8693d5b6768a37cd421385caddc0d5a263a592a52e98b2a79369977e2691e2924a033e43db668'
